09c83746badb15cc24f23f13d55aa88cd40fc3fd
vergilgeek <vergilgeek@aliyun.com>
2020-06-28 13:48:25 +0800

    20. 有效的括号

---
+++ src/main/java/leetcode/solution/ValidBracket.java
@@ -0,0 +1,37 @@
+package leetcode.solution;
+
+import java.util.Stack;
+
+/**
+ * 20. 有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * <p>
+ * 有效字符串需满足：
+ * <p>
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ */
+public class ValidBracket {
+    public static void main(String[] args) {
+        System.out.println(solution("{[]}"));
+    }
+
+    public static boolean solution(String s) {
+        if (null == s || s.isEmpty()) {
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if(c=='(')
+                stack.push(')');
+            else if(c=='{')
+                stack.push('}');
+            else if(c=='[')
+                stack.push(']');
+            else if(stack.empty()||c!=stack.pop())
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
