2220ee95655e892b3e3035857f3314d26bb07152
Vergilgeek <251627752@qq.com>
2020-09-27 09:23:08 +0800

    235. 二叉搜索树的最近公共祖先

---
+++ src/main/java/leetcode/solution/LowestCommonAncestor.java
@@ -0,0 +1,41 @@
+package leetcode.solution;
+
+/**
+ * 235. 二叉搜索树的最近公共祖先
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，
+ * 最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ */
+public class LowestCommonAncestor {
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 从根节点开始遍历；
+     * 如果当前节点的值大于 pp 和 qq 的值，说明 pp 和 qq 应该在当前节点的左子树，因此将当前节点移动到它的左子节点；
+     * 如果当前节点的值小于 pp 和 qq 的值，说明 pp 和 qq 应该在当前节点的右子树，因此将当前节点移动到它的右子节点；
+     * 如果当前节点的值不满足上述两条要求，那么说明当前节点就是「分岔点」。此时，pp 和 qq 要么在当前节点的不同的子树中，要么其中一个就是当前节点。
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode result = root;
+        while (true) {
+            if (result.val > p.val && result.val > q.val) {
+                result = result.left;
+            } else if (result.val < p.val && result.val < q.val) {
+                result = result.right;
+            } else {
+                break;
+            }
+        }
+        return result;
+    }
+}
